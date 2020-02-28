95c4bf610dbc0365abde347b955909ffd9e4d23b
damianb53 <damianb53@outlook.com>
2020-02-28 01:54:50 +0100

    Add css and scss support

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // style-loader
+                    { loader: 'style-loader' },
+                    // css-loader
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                    // sass-loader
+                    { loader: 'sass-loader' },
+                ],
+            },
+        ],
+    },
     entry: {
         app: './src/index.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Document',
+            template: 'src/index.html',
         }),
     ],
     output: {
